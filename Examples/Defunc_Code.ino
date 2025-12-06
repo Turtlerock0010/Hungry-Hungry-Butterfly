2d488c3758080f653dab1bc1c966f8f2874f54f1
Turtlerock0010 <danielrprincipe@gmail.com>
2025-12-06 17:27:30 -0500

    Add initial Defunc_Code.ino file with comments

---
+++ Examples/Defunc_Code.ino
@@ -0,0 +1,11 @@
+// Adjust Height
+    /* if (PestoLink.buttonHeld(1) || PestoLink.keyHeld(Key::C)) {
+      pivotingServo.write(0);
+    } else {
+      pivotingServo.write(120); // Set it back to 135
+    }
+    */
+    
+
+    // Adjusts Intake Arm Angle (Possibly might be removed)
+    //intakeServo.write(PestoLink.buttonHeld(0) || PestoLink.keyHeld(Key::V) ? 100 : 180);
